ebe4a49df7676f09210d1a27c239b0964c66bf1d
Nilton da Silva Junior <tonsti@gmail.com>
2020-09-17 20:45:22 +0200

    Get Records

--- backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -2,13 +2,13 @@ package com.devsuperior.dspesquisa.services;
 
 import java.time.Instant;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dspesquisa.dto.GameDTO;
 import com.devsuperior.dspesquisa.dto.RecordDTO;
 import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
 import com.devsuperior.dspesquisa.entities.Game;
@@ -32,12 +32,17 @@ public class RecordService {
 		entity.setName(dto.getName());
 		entity.setAge(dto.getAge());
 		entity.setMoment(Instant.now());
-		
+		 
 		Game game = gameRepository.getOne(dto.getGameId());
 		entity.setGame(game);
 		
 		entity = repository.save(entity);
 		return new RecordDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
 	}
 
 }
