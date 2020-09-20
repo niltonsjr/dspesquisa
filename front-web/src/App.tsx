25352e2bd4df626bd4dcaa62ca9e67384898228c
Nilton da Silva Junior <tonsti@gmail.com>
2020-09-20 12:48:24 +0200

    Add header component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-   <h1>Hello Semana DEVSuperior!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
